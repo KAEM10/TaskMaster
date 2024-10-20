079ce4aa4487f8f8452b97cad77f1421f011a5b9
cjaneth <42422255+cjaneth@users.noreply.github.com>
2024-10-20 18:33:22 -0500

    UPDATE:dialogoConfirmacionRegistroTarea

--- app/src/main/java/edu/unicauca/taskmaster/ui/screens/create/CreateTaskViewModel.kt
+++ app/src/main/java/edu/unicauca/taskmaster/ui/screens/create/CreateTaskViewModel.kt
@@ -6,6 +6,13 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import android.app.AlertDialog
 import android.content.Context
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import edu.unicauca.taskmaster.R
+
 class CreateTaskViewModel: ViewModel() {
     // Estado del formulario
     private val _uiState = MutableStateFlow(CreateTaskUiState())
@@ -37,24 +44,30 @@ class CreateTaskViewModel: ViewModel() {
         val selectedDays = _uiState.value.selectedDays.joinToString(", ")
         val reminderType = _uiState.value.reminderType
 
-        // Aquí puedes implementar la lógica para mostrar la alerta
         if (taskName.isNotEmpty() && selectedDays.isNotEmpty() && reminderType.isNotEmpty()) {
             val message = "Tarea: $taskName\nDías: $selectedDays\nRecordatorio: $reminderType"
             // Llama a un método para mostrar la alerta
-            showAlert(context, message)  // Asegúrate de pasar el contexto correcto
+            showAlert(context, message,true)
         }else{
             val message = "Todos los campos son requeridos"
             // Llama a un método para mostrar la alerta
-            showAlert(context, message)  // Asegúrate de pasar el contexto correcto
+            showAlert(context, message,false)
         }
     }
 
-    private fun showAlert(context: Context, message: String) {
+    private fun showAlert(context: Context, message: String ,save:Boolean) {
         val builder = AlertDialog.Builder(context)
         builder.setTitle("Guardar Tarea")
         builder.setMessage(message)  // Usa el mensaje dl argumento
         builder.setPositiveButton("Aceptar") { dialog, which ->
+            if(save){
+                //TODO: aca guardar la info en la bd
+               // Toast.makeText(context, "Se ha guardado la Tarea", Toast.LENGTH_LONG).show()
 
+                showCustomToast(context, "Se ha guardado la Tarea")
+                resetForm()
+                showCustomToast(context, "Se ha guardado la Tarea")
+            }
         }
         builder.setNegativeButton("Cancelar") { dialog, which ->
             dialog.dismiss()
@@ -62,6 +75,30 @@ class CreateTaskViewModel: ViewModel() {
         val alertDialog = builder.create()
         alertDialog.show()
     }
+    private fun showCustomToast(context: Context, message: String) {
+        val inflater = LayoutInflater.from(context)
+        val layout: View = inflater.inflate(R.layout.toast_custom, null)
 
+        // Establece el texto del mensaje
+        val textView = layout.findViewById<TextView>(R.id.toast_text)
+        textView.text = message
 
+        // Crea el Toast
+        val toast = Toast(context)
+        toast.duration = Toast.LENGTH_SHORT
+        toast.view = layout
+
+        // Cambia la posición del Toast
+        toast.setGravity(Gravity.TOP or Gravity.CENTER_HORIZONTAL, 0, 100)
+
+        // Muestra el Toast
+        toast.show()
+    }
+    private fun resetForm() {
+        _uiState.value = CreateTaskUiState(
+            taskName = "",
+            selectedDays = emptyList(),
+            reminderType = ""
+        )
+    }
 }
